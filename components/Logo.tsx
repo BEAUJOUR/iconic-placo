9a9eec8291a03da3efe98948b4e6f73c49f93bba
BEAUJOUR <beaujourbicool@gmail.com>
2026-09-19 12:12:51 +0200

    revert overly tight logo crop

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -21,7 +21,7 @@ export function Logo({ size = "md" }: LogoProps) {
         alt="Iconic P Solution"
         fill
         sizes="96px"
-        className="object-cover"
+        className="scale-[1.42] object-cover"
         priority={size !== "lg"}
       />
     </span>
